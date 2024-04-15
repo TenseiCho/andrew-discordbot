eb9156102f5fac9cceefd15d11474a8f33398835
John Garner <zellasphere@gmail.com>
2024-04-14 22:17:03 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+const PREFIX = '!'; // Change this to your desired prefix
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', (message) => {
+  // ... (rest of the code remains the same)
+});
+
+client.login(process.env.BOT_TOKEN);
